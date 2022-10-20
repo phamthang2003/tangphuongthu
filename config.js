56250afaa6f4fdbc67d4be0a97e37c614465c816
phamthang2003 <116180596+phamthang2003@users.noreply.github.com>
2022-10-20 21:04:08 +0700

    Add files via upload
    anhthu

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'bạn Thu';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng bạn Thu nekkk';
+const contentCard = 'chúc bạn Thu có ngày 20/10 vui vẻ, ý nghĩa, chúc bạn Thu luôn xinh đẹp, luôn mạnh khỏe, thành công trong mọi việc nhé';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'anhthu.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
